f8e285a03926aae7fad05aedcfb19dd12b8dd40d
toufiq007 <t.limon2342@gmail.com>
2024-11-21 10:01:02 +0600

    toggle login or logout button by the condition of user login or logout

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import LogoIcon from "../../assets/logo.svg";
 import { useAuth } from "../../hooks/useAuth";
 const Header = () => {
-  const { setAuth } = useAuth();
+  const { setAuth, auth } = useAuth();
   const navigate = useNavigate();
   const handleLogout = () => {
     setAuth({});
@@ -13,19 +13,21 @@ const Header = () => {
       <header className="flex justify-between items-center mb-12">
         <img src={LogoIcon} />
         <div>
-          <Link
-            to="/login"
-            className="px-4 py-2 rounded hover:bg-primary hover:text-white transition-colors"
-          >
-            Login
-          </Link>
-
-          <button
-            onClick={handleLogout}
-            className="px-4 py-2 rounded hover:bg-primary hover:text-white transition-colors"
-          >
-            Logout
-          </button>
+          {auth?.data?.user ? (
+            <button
+              onClick={handleLogout}
+              className="px-4 py-2 rounded hover:bg-primary hover:text-white transition-colors"
+            >
+              Logout
+            </button>
+          ) : (
+            <Link
+              to="/login"
+              className="px-4 py-2 rounded hover:bg-primary hover:text-white transition-colors"
+            >
+              Login
+            </Link>
+          )}
         </div>
       </header>
     </>
